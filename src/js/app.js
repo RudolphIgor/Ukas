fcaf94c9cc41bfbdffcf940c543556c14d4659b0
Rudolph <azazil47@gmail.com>
2022-08-08 15:32:45 +0700

    Реализована плавная прокрутка по меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as slowlyScroll from "./modules/slowly_scroll.js"
 
 flsFunctions.isWebp();
+slowlyScroll.slScroll();
---
+++ src/js/modules/slowly_scroll.js
@@ -0,0 +1,23 @@
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+export function slScroll() {
+	if (menuLinks.length > 0) {
+		menuLinks.forEach(element => {
+			element.addEventListener('click', onMenuLinkClick)
+		});
+	}
+}
+
+function onMenuLinkClick(e) {
+	const menuLink = e.target;
+	if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+		const gotoBlock = document.querySelector(menuLink.dataset.goto);
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY;
+
+		window.scroll({
+			top:gotoBlockValue,
+			behavior: "smooth"
+		})
+		e.preventDefault();
+	}
+}
