1b22a8b57ddaa6ff415b4c08a03c53c945c5e95d
Rudolph <azazil47@gmail.com>
2022-08-09 11:22:18 +0700

    Реализован механизм появления меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as slowlyScroll from "./modules/slowly_scroll.js"
+import * as burger from "./modules//burger.js"
 
 flsFunctions.isWebp();
 slowlyScroll.slScroll();
+burger.iconsactive();
