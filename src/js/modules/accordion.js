fa8685894f5cb1ed4908ec08c80a6e3909b0fe37
Rudolph <azazil47@gmail.com>
2022-08-12 11:22:49 +0700

    РЕализован механизм акардиона не без костылей

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,9 +1,18 @@
 export function swipe () {
 	document.querySelectorAll('.accordion__item').forEach(element => {
+		
 		element.addEventListener('click', () => {
-			element.classList.toggle('active');
-			const parent = element.childNodes;
-			console.log(parent);
+			// element.classList.add('active');
+			document.querySelectorAll('.accordion__item').forEach(element => {
+				element.style.maxHeight = '71px';
+			})
+			element.style.maxHeight = element.scrollHeight + 'px';
+			
+
+
+
+
+			console.log(element.style.maxHeight);
 		});
 	});
 }
