df7448aa37b74e7b88f6c94946638b8a789e4250
Rudolph <azazil47@gmail.com>
2022-08-12 12:12:25 +0700

    Реализован механизм акардиона с блэкджеком но пока без шлюх

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -2,17 +2,19 @@ export function swipe () {
 	document.querySelectorAll('.accordion__item').forEach(element => {
 		
 		element.addEventListener('click', () => {
-			// element.classList.add('active');
-			document.querySelectorAll('.accordion__item').forEach(element => {
+			if (element.classList.contains('active')){
+				element.classList.remove('active');
 				element.style.maxHeight = '71px';
-			})
-			element.style.maxHeight = element.scrollHeight + 'px';
-			
-
-
-
-
+			} else {
+				document.querySelectorAll('.accordion__item').forEach(el => {
+					el.classList.remove('active');
+					el.style.maxHeight = '71px';
+				})
+				element.classList.add('active');
+				element.style.maxHeight = element.scrollHeight + 'px';
+			}
 			console.log(element.style.maxHeight);
 		});
+		
 	});
 }
