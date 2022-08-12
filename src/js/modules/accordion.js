5d712203b75585e77fdcf701b72ee89e1cfa59de
Rudolph <azazil47@gmail.com>
2022-08-12 12:26:20 +0700

    Добавлен контент в аккардион

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -2,19 +2,21 @@ export function swipe () {
 	document.querySelectorAll('.accordion__item').forEach(element => {
 		
 		element.addEventListener('click', () => {
-			if (element.classList.contains('active')){
-				element.classList.remove('active');
-				element.style.maxHeight = '71px';
-			} else {
-				document.querySelectorAll('.accordion__item').forEach(el => {
-					el.classList.remove('active');
-					el.style.maxHeight = '71px';
-				})
-				element.classList.add('active');
-				element.style.maxHeight = element.scrollHeight + 'px';
+			if(!element.classList.contains('no-toch')){
+				if (element.classList.contains('active')){
+					element.classList.remove('active');
+					element.style.maxHeight = '71px';
+				} else {
+					document.querySelectorAll('.accordion__item').forEach(el => {
+						el.classList.remove('active');
+						el.style.maxHeight = '71px';
+					})
+					element.classList.add('active');
+					element.style.maxHeight = element.scrollHeight + 'px';
+				}
 			}
 			console.log(element.style.maxHeight);
 		});
-		
+
 	});
 }
