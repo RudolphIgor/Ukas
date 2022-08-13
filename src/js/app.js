08a15a85324102fa8190095365ec9cc0c6c0762b
Rudolph <azazil47@gmail.com>
2022-08-13 14:01:15 +0700

    Добавлен свайпер

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@ import * as flsFunctions from "./modules/functions.js";
 import * as slowlyScroll from "./modules/slowly_scroll.js"
 import * as burger from "./modules//burger.js"
 import * as accordion from "./modules/accordion.js"
+import * as slider from "./modules/swiper.js"
 
 flsFunctions.isWebp();
 slowlyScroll.slScroll();
 burger.iconsactive();
 accordion.swipe();
+slider.startslider();
