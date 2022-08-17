ffad4a546314f761b80e99cf50585bca94aeb828
Rudolph <azazil47@gmail.com>
2022-08-17 15:09:36 +0700

    Пофикшено меню

--- src/js/modules/slowly_scroll.js
+++ src/js/modules/slowly_scroll.js
@@ -1,5 +1,7 @@
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-
+const icon = document.querySelector('.menu__icon');
+const menu = document.querySelector('.menu__body');
+const header = document.querySelector('.header__container');
 
 export function slScroll() {
 	console.log(menuLinks);
@@ -14,12 +16,15 @@ function onMenuLinkClick(e) {
 	const menuLink = e.target;
 	if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 		const gotoBlock = document.querySelector(menuLink.dataset.goto);
-		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY;
-
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - 300; //300 - Величина паддинга раскрывающейся под меню в шапке
+		
 		window.scroll({
 			top:gotoBlockValue,
 			behavior: "smooth"
 		})
 		e.preventDefault();
+		icon.classList.remove('menu__icon_active');
+		menu.classList.remove('menu__body_active');
+		header.classList.remove('header__container_active');
 	}
 }
