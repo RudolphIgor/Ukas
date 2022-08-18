de0749c24601ca0c4d10830bda3701207a388a10
Rudolph <azazil47@gmail.com>
2022-08-18 11:55:39 +0700

    Написан скрипт отправки и валидации формы с функцией контроля отправки

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import * as burger from "./modules//burger.js"
 import * as accordion from "./modules/accordion.js"
 import * as slider from "./modules/swiper.js"
 import * as arrow from "./modules/card_arrow.js"
+import * as bot from "./modules/bot.js"
 
 flsFunctions.isWebp();
 slowlyScroll.slScroll();
